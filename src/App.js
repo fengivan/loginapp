f93b2f6637bc00bc8e025eae6ab3ae9ed8f50215
fenivan@gmail.com <fenivan@gmail.com>
2022-04-09 02:26:27 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import LoginHooks from './components/LoginHooks';
+import LogoutHooks from './components/LogoutHooks';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Please log in with google</h2>
+      <Login />
+      <br />
+      <Logout />
+      
+      
+    
     </div>
   );
 }
