1b54d815868d96b8dfe312d5419da03fc3ace2a8
fenivan@gmail.com <fenivan@gmail.com>
2022-05-22 21:16:00 -0500

    edits
    edits

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import React from 'react';
 import './App.css';
 import Login from './components/Login';
 import Logout from './components/Logout';
-import LoginHooks from './components/LoginHooks';
-import LogoutHooks from './components/LogoutHooks';
+import BackgroundImage from './assets/images/background.png'
+
 
 function App() {
   return (
-    <div className="App">
-      <h2>Please log in with google</h2>
-      <Login />
-      <br />
-      <Logout />
+    <header style={ HeaderStyle }>
+      <div className="App" >
+        <h2>Log in with Google!</h2>
+        <Login />
+        <br />
+        <Logout />
+        
+        
       
-      
-    
-    </div>
+      </div>
+    </header>
   );
 }
 
 export default App;
+
+const HeaderStyle = {
+  width: "100%",
+  height: "100vh",
+  background: `url(${BackgroundImage})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover"
+}
+
